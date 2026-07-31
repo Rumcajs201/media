20f246ba5cde854705b505c9a7a3ce90c27490b7
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-31 07:52:44 +0200

    Odśwież panel konta administratora i automatyczne dane MP3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="rumcajs-media-v2";
+const CACHE="rumcajs-media-v3";
 const CORE=["./","./index.html","./style.css","./app.js","./media.json","./manifest.webmanifest","./covers/default-cover.svg","./assets/icons/icon-192.png","./assets/icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
